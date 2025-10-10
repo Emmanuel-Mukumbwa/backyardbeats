fcf25f928ca72ff774cc8f1f8d1bea333ce692d9
Emmanuel-Mukumbwa <132208620+Emmanuel-Mukumbwa@users.noreply.github.com>
2025-10-10 05:06:06 +0000

    Update Sequelize config for backyardbeatsDB and new MySQL password

--- src/server/db.js
+++ src/server/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('backyard_beats', 'root', 'password', {
+const sequelize = new Sequelize('backyardbeatsDB', 'root', '123456', {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
